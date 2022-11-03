9466eb7d0d1769dbce75de76d841a1b19c551cb4
redgroul <redgroulman@gmail.com>
2022-11-03 19:24:55 +0300

    fix conflict and merge caf tag LA.UM.8.2.r2-06200-sdm660.0

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1,4 +1,8 @@
-/* Copyright (c) 2002,2007-2019, The Linux Foundation. All rights reserved.
+<<<<<<< HEAD
+=======
+/* Copyright (c) 2002,2007-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+>>>>>>> 4ad6c9ec28e7e091fe7fb8f68d84f86be1a6d076
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1015,6 +1019,8 @@ static int adreno_probe(struct platform_device *pdev)
 
 	adreno_debugfs_init(adreno_dev);
 	adreno_profile_init(adreno_dev);
+
+	adreno_dev->perfcounter = false;
 
 	adreno_sysfs_init(adreno_dev);
 
